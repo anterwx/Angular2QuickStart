c2d0f1b73d6e0ab4fb6c458d688dca02a0d6556b
王星 <wangxing@bjzjmy.com>
2016-09-30 16:19:55 +0800

    介绍Jit和AOT两种引导方式

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,15 @@
+//通过即时（ JiT ）编译器动态引导
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+//使用预编译器（ AoT - Ahead-Of-Time ）进行静态引导
+// The browser platform without a compiler
+//import { platformBrowser } from '@angular/platform-browser';
+
+// The app module factory produced by the static offline compiler
+//import { AppModuleNgFactory } from './app.module.ngfactory';
+
+// Launch with the app module factory.
+//platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
