e3e6a6ae5d62f70707588223c8a12a1ac9b73f6c
王星 <wangxing@bjzjmy.com>
2016-09-30 14:35:48 +0800

    Revert "修改hero.service.ts中获取列表接口，通过http获取数据"
    This reverts commit 1660865293277306c6d0a125c3bd7eff0770eee3.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,7 @@
-import { Injectable }    from '@angular/core';
-import { Headers, Http } from '@angular/http';
-
-import 'rxjs/add/operator/toPromise';
+import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
-
+import { HEROES } from './mock-heroes';
 
 /**
  * 服务可被注入
@@ -16,21 +13,9 @@ export class HeroService{
         return HEROES;
     }*/
 
-    /*getHeroes() : Promise<Hero[]>{
+    getHeroes() : Promise<Hero[]>{
         return Promise.resolve(HEROES);
-    }*/
-
-    private heroesUrl = 'app/heroes';  // URL to web api
-
-    constructor(private http: Http) { }
-
-    getHeroes(): Promise<Hero[]> {
-        return this.http.get(this.heroesUrl)
-            .toPromise()
-            .then(response => response.json().data as Hero[])
-            .catch(this.handleError);
     }
-
 
     getHeroesSlowly(): Promise<Hero[]>{
         return new Promise<Hero[]>(resolve => setTimeout(resolve,2000)).then(() =>this.getHeroes());
@@ -41,9 +26,5 @@ export class HeroService{
             .then(heroes => heroes.find(hero => hero.id === id));
     }
 
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
 
 }
